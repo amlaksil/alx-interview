3db3f9fd40edfa5d55090466ebf5087536c25a06
amlaksil <silamlakdesye@gmail.com>
2023-10-20 02:59:01 +0300

    Refactor

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,13 +11,11 @@ pattern2 = r'\"GET\s\/projects\/260\sHTTP\/1\.1\"\s\d\d\d\s\d+'
 pattern = pattern1 + pattern2
 line_number = 0
 file_size = 0
-count_200 = count_301 = count_400 = count_401 = 0
-count_403 = count_404 = count_405 = count_500 = 0
 
 dict_t = {
-        200: count_200, 301: count_301, 400: count_400,
-        401: count_401, 403: count_403, 404: count_404,
-        405: count_405, 500: count_500
+        200: 0, 301: 0, 400: 0,
+        401: 0, 403: 0, 404: 0,
+        405: 0, 500: 0
         }
 
 try:
@@ -32,31 +30,17 @@ try:
         else:
             list_t = line.split()
             stat_code = int(list_t[7])
-            if stat_code == 200:
-                dict_t[200] += 1
-            elif stat_code == 301:
-                dict_t[301] += 1
-            elif stat_code == 400:
-                dict_t[400] += 1
-            elif stat_code == 401:
-                dict_t[401] += 1
-            elif stat_code == 403:
-                dict_t[403] += 1
-            elif stat_code == 404:
-                dict_t[404] += 1
-            elif stat_code == 405:
-                dict_t[405] += 1
-            elif stat_code == 500:
-                dict_t[500] += 1
+            if stat_code in dict_t:
+                dict_t[stat_code] += 1
             file_size += int(list_t[8])
         if line_number == 10:
             print(f"File size: {file_size}")
-            for key, value in dict_t.items():
+            for key, value in sorted(dict_t.items()):
                 if value != 0:
                     print(f'{key}: {value}')
             line_number = 0
 except KeyboardInterrupt:
     print(f"File size: {file_size}")
-    for key, value in dict_t.items():
+    for key, value in sorted(dict_t.items()):
         if value != 0:
             print(f'{key}: {value}')
