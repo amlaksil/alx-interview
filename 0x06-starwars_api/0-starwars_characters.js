577cbed40bb0ade10b86f16068b9536299e56b0e
amlaksil <silamlakdesye@gmail.com>
2023-11-10 01:36:44 +0300

    Refactor and update display order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,40 @@
 #!/usr/bin/node
 
 const request = require('request');
+const { promisify } = require('util');
 
-// Get the movie ID from command-line arguments
+// Convert the request function to a promise-based version
+const requestPromise = promisify(request);
+
+// Function to fetch character details asynchronously
+async function fetchCharacterDetails (characterUrl) {
+  try {
+    const response = await requestPromise(characterUrl);
+    const character = JSON.parse(response.body);
+    console.log(character.name);
+  } catch (error) {
+    console.error('Error fetching character:', error);
+  }
+}
+
+// Function to fetch movie details and character names
+async function fetchMovieDetails () {
+  try {
+    const response = await requestPromise(`https://swapi.dev/api/films/${movieId}/`);
+    const movie = JSON.parse(response.body);
+    const characters = movie.characters;
+
+    // Fetch character details in order
+    for (const characterUrl of characters) {
+      await fetchCharacterDetails(characterUrl);
+    }
+  } catch (error) {
+    console.error('Error fetching movie:', error);
+  }
+}
+
+// Get the movie ID from the command-line argument
 const movieId = process.argv[2];
 
-// Make a request to the Star Wars API to fetch the movie details
-request(`https://swapi.dev/api/films/${movieId}/`, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const movie = JSON.parse(body);
-    const characterUrls = movie.characters;
-
-    // Fetch each character's details
-    characterUrls.forEach((characterUrl) => {
-      request(characterUrl, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
-      });
-    });
-  }
-});
+// Invoke the main function
+fetchMovieDetails();
