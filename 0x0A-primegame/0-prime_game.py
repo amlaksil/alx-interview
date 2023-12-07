b02ad632d1503854b05bc4863288963dcd738855
amlaksil <silamlakdesye@gmail.com>
2023-12-07 16:12:05 +0300

    Update isWinner

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,49 +1,57 @@
 #!/usr/bin/python3
-
 """
-This module contains a function called isWinner which
-helps us to determine the winner of the game.Description for the game:
-Maria and Ben are playing a game. Given a set of consecutive integers starting
-from 1 up to and including n, they take turns choosing a prime number from the
-set and removing that number and its multiples from the set. The player that
-cannot make a move loses the game. Assuming Maria always goes first and both
-players play optimally.
+This module contains functions needed to determine the winner of the game.
+Description for the game: Maria and Ben are playing a game. Given a set of
+consecutive integers starting from 1 up to and including n, they take turns
+choosing a prime number from the set and removing that number and its multiples
+from the set. The player that cannot make a move loses the game. Assuming Maria
+always goes first and both players play optimally.
 """
 
 
-def isWinner(x, nums):
+def is_prime(num):
+    """
+    Check if a number is prime.
+
+    Args:
+        num (int): The number to check.
+
+    Returns:
+        bool: True if the number is prime, False otherwise.
+    """
+    if num < 2:
+        return False
+    for i in range(2, num + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def isWinner(_, nums):
     """
     Determine the winner of the prime game for each round.
 
     Args:
-        x (int): The number of rounds. (Unused)
+        _ (int): The number of rounds. (Unused)
         nums (list): A list of integers representing n for each round.
 
     Returns:
         str or None: The name of the player (Maria or Ben) that won the most
         rounds, or None if the winner cannot be determined.
     """
-    if not nums or x < 1:
-        return None
-    max_num = max(nums)
+    maria_wins = 0
+    ben_wins = 0
 
-    my_filter = [True for _ in range(max(max_num + 1, 2))]
-    for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if not my_filter[i]:
-            continue
-        for j in range(i * i, max_num + 1, i):
-            my_filter[j] = False
-    my_filter[0] = my_filter[1] = False
-    y = 0
-    for i in range(len(my_filter)):
-        if my_filter[i]:
-            y += 1
-        my_filter[i] = y
-    player1 = 0
-    for x in nums:
-        player1 += my_filter[x] % 2 == 1
-    if player1 * 2 == len(nums):
-        return None
-    if player1 * 2 > len(nums):
+    for n in nums:
+        primes = [num for num in range(2, n + 1) if is_prime(num)]
+
+        if len(primes) % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
         return "Maria"
-    return "Ben"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    return None
